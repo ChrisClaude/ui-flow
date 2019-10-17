37377f2cd66bc6d03bd3e431f2b1c8ef1e89821a
ChClaude <christ.tchambila@gmail.com>
2019-10-17 13:21:33 +0200

    project setup

--- uiflow/uiflow/settings.py
+++ uiflow/uiflow/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'uiflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ui_flow',
+        'USER': 'root',
+        'PASSWORD': 'Cla@5027jesuschrist',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
